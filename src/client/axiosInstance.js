afe8c765a13d924316c92b43727f884456b88149
hissoune <khalidhissoune962@gmail.com>
2024-11-28 06:36:35 +0100

    Add : adding the client and use the axios interseptor

---
+++ src/client/axiosInstance.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_EXPRESS_BACKEND ,
+});
+
+axiosInstance.interceptors.request.use(
+  function (config) {
+    const token = localStorage.getItem('token'); 
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+      config.headers['Content-Type']='multipart/form-data'
+    }
+
+    return config;
+  },
+  function (error) {
+     
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
