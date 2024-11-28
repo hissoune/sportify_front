1f592840a41b8cd169b77e323c996617943cb85c
hissoune <khalidhissoune962@gmail.com>
2024-11-28 17:07:21 +0100

    Modified Dashboard.jsx to improve user interface and functionality

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,26 +1,20 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { logout } from "../redux/slices/AuthSlice";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 
 const Dashboard = () => {
-  const { user } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
 
-  const handleLogout = () => {
-    dispatch(logout());
-    navigate("/login");
-  };
+
 
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      {/* Sidebar */}
+    <div className="flex min-h-screen bg-gradient-to-t from-slate-400 to-slate-200">
+    
       <Sidebar />
 
-      {/* Main Content Area */}
-      <div className="flex-1 ml-64 p-6"> {/*  ml-64 ensures content is not hidden under the sidebar */}
+     
+      <div className="flex-1 ml-64 p-6">
         <Outlet />
       </div>
     </div>
