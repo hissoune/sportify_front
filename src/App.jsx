5c7c5954dba47da71e0bb8cbf58570d883675f31
hissoune <khalidhissoune962@gmail.com>
2024-11-28 14:50:11 +0100

    Add : add the links of the added pages in the app insid the dash

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Login from "./components/auth/Login"
 import Register from "./components/auth/Register"
 import Dashboard from "./pages/Dashboard"
 import ProtectedRoute from "./helpers/ProtectedRoute"
+import Statistics from "./pages/Statistics"
+import EventPage from "./pages/EventsPage"
+import UsersPage from "./pages/UsersPage"
 
 
 function App() {
@@ -14,7 +17,9 @@ function App() {
       <Route path='/register' element={<Register />} />
 
       <Route  path='/dashboard' element={<ProtectedRoute role='organizer'><Dashboard /></ProtectedRoute>}>
-
+       <Route index element={<Statistics/>} />
+       <Route  path="/dashboard/events" element={<EventPage />} />
+       <Route  path="/dashboard/users" element={<UsersPage />} />
       </Route>
 
      
