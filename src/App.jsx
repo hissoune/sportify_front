c78014b338d1fd82baedcc3ef791d458465919ad
hissoune <khalidhissoune962@gmail.com>
2024-11-28 10:31:27 +0100

    Add : add the dashboard roote and protect it

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes,Route } from "react-router-dom"
 import Login from "./components/auth/Login"
 import Register from "./components/auth/Register"
+import Dashboard from "./pages/Dashboard"
+import ProtectedRoute from "./helpers/ProtectedRoute"
 
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
    <Routes>
     <Route path='/' element={<Login/>} />
     <Route path='/register' element={<Register/>} />
+    <Route path='/dashboard' element={<ProtectedRoute role='organizer'><Dashboard/></ProtectedRoute> } />
    </Routes> 
   )
 }
