e6bf0244aab93a4b82304980913f12abe74fd712
hissoune <khalidhissoune962@gmail.com>
2024-11-28 17:07:23 +0100

    Refined UsersPage.jsx for better user management functionality

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -106,7 +106,6 @@ const UsersPage = () => {
         ))}
       </div>
 
-      {/* Modal */}
       {showModal && (
         <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex items-center justify-center">
           <div className="bg-white p-6 rounded-lg w-96">
