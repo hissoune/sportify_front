064bb998e5f5e1e78658946600ffd17eebea4359
hissoune <khalidhissoune962@gmail.com>
2024-11-28 22:28:07 +0100

    Modified store.js to include EventSlice in the Redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/AuthSlice';
-
+import eventReducer from './slices/EventSlice'
 const store = configureStore({
   reducer: {
     auth: authReducer, 
+    events: eventReducer, 
   },
 });
 
