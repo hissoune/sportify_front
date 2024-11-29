dbe17c3d007eb8570f9df13794ae7b3048cadf2e
hissoune <khalidhissoune962@gmail.com>
2024-11-29 14:04:09 +0100

    Refactor ParticipantService for improved participant data handling

--- src/services/ParticipantService.js
+++ src/services/ParticipantService.js
@@ -10,7 +10,19 @@ getAllParticipants:async ()=>{
         
         return response.data; 
       } catch (error) {
-        console.error('Login failed:', error);
+        console.error('getting users  failed:', error);
+        throw error; 
+      }
+},
+createParticipant:async (formData)=>{
+    
+    try {
+        const response = await axiosInstance.post('/users',formData);
+       
+        
+        return response.data; 
+      } catch (error) {
+        console.error('creating user failed:', error);
         throw error; 
       }
 }
