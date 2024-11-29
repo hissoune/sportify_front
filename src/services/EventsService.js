9519ebabe5673cff33e0d4c54e2cb6c1cbfebdb4
hissoune <khalidhissoune962@gmail.com>
2024-11-29 05:12:33 +0100

    Updated EventsService.js to include new API calls for event CRUD operations

--- src/services/EventsService.js
+++ src/services/EventsService.js
@@ -6,7 +6,19 @@ const EventsService ={
 getAllEvents:async () =>{
     try {
         const response = await axiosInstance.get('/events');
-       ;
+       
+        
+        return response.data; 
+      } catch (error) {
+        console.error('Login failed:', error);
+        throw error; 
+      }
+},
+
+createEvent:async (formdata)=>{
+    try {
+        const response = await axiosInstance.post('/events',formdata);
+       
         
         return response.data; 
       } catch (error) {
